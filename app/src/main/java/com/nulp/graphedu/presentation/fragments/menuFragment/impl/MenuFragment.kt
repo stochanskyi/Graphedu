e5cd02f4a3292f291da8eb0a1d51a5af38285cf9
Marian Stochanskyi <stochan.andr@gmail.com>
2020-11-29 11:16:23 +0200

    Revert "Implement navigation from start fragment to tabs fragment"
    This reverts commit debac672

--- app/src/main/java/com/nulp/graphedu/presentation/fragments/menuFragment/impl/MenuFragment.kt
+++ app/src/main/java/com/nulp/graphedu/presentation/fragments/menuFragment/impl/MenuFragment.kt
@@ -1,11 +1,8 @@
 package com.nulp.graphedu.presentation.fragments.menuFragment.impl
 
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.nulp.graphedu.R
 import com.nulp.graphedu.presentation.common.mvp.BaseFragment
 import com.nulp.graphedu.presentation.fragments.menuFragment.MenuContract.*
-import kotlinx.android.synthetic.main.fragment_menu.*
 import org.koin.android.ext.android.inject
 
 class MenuFragment : BaseFragment<PresenterContract>(R.layout.fragment_menu), ViewContract {
@@ -23,8 +20,6 @@ class MenuFragment : BaseFragment<PresenterContract>(R.layout.fragment_menu), Vi
     }
 
     override fun initViews() {
-        textStart.setOnClickListener {
-            findNavController().navigate(R.id.action_start_to_tabs)
-        }
+        //TODO
     }
 }
--- app/src/main/java/com/nulp/graphedu/presentation/fragments/tabFlowFragment/TabsFlowFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.nulp.graphedu.presentation.fragments.tabFlowFragment
-
-import androidx.fragment.app.Fragment
-import com.nulp.graphedu.R
-
-class TabsFlowFragment : Fragment(R.layout.fragment_tabs_flow)
