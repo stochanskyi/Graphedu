3503d81ba0df4716f4779b5ae05d3fae935f9db7
Mallivance <ashpek01@gmail.com>
2021-01-03 16:43:38 +0200

    Revert "Remove start affine fragment"
    This reverts commit 3358ea20

--- app/src/main/java/com/nulp/graphedu/presentation/fragments/mainNavigation/navigationController/NavigationController.kt
+++ app/src/main/java/com/nulp/graphedu/presentation/fragments/mainNavigation/navigationController/NavigationController.kt
@@ -3,6 +3,7 @@ package com.nulp.graphedu.presentation.fragments.mainNavigation.navigationContro
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.nulp.graphedu.presentation.fragments.mainNavigation.navigationController.navItems.AffineNavigationItem
 import com.nulp.graphedu.presentation.fragments.mainNavigation.navigationController.navItems.ColorsNavigationItem
 import com.nulp.graphedu.presentation.fragments.mainNavigation.navigationController.navItems.FractalsNavigationItem
 import com.nulp.graphedu.presentation.fragments.mainNavigation.navigationController.navItems.NavigationItem
@@ -14,7 +15,8 @@ class NavigationController(
 
     private val items: List<NavigationItem> = listOf(
         FractalsNavigationItem(),
-        ColorsNavigationItem()
+        ColorsNavigationItem(),
+        AffineNavigationItem()
     )
 
     init {
